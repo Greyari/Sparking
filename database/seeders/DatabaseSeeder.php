154b7f8e747a4de3a58cad92755488b59ba70ddc
Greyari <159263690+Greyari@users.noreply.github.com>
2026-02-24 13:25:54 +0700

    update seeder bagian logika jadi update or create 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,28 +13,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::updateOrCreate([
-            'identitas' => '12345678',
-            'email' => 'admin@gmail.com',
-            'jenis_pengguna' => 'karyawan',
-            'nama' => 'Admin',
-            'password' => Hash::make('123'),
-            'jenis_kendaraan' => 'motor',
-            'no_plat' => 'BP1234AA',
-            'role' => 'admin',
-            'status' => 'aktif',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'admin@gmail.com'], // kondisi unik
+            [
+                'identitas' => '12345678',
+                'jenis_pengguna' => 'karyawan',
+                'nama' => 'Admin',
+                'password' => Hash::make('123'),
+                'jenis_kendaraan' => 'motor',
+                'no_plat' => 'BP1234AA',
+                'role' => 'admin',
+                'status' => 'aktif',
+            ]
+        );
 
-        User::updateOrCreate([
-            'identitas' => '1234567',
-            'email' => 'dosen@gmail.com',
-            'jenis_pengguna' => 'karyawan',
-            'nama' => 'Dosen',
-            'password' => Hash::make('123'),
-            'jenis_kendaraan' => 'motor',
-            'no_plat' => 'BP12341A',
-            'role' => 'pengguna',
-            'status' => 'aktif',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'dosen@gmail.com'],
+            [
+                'identitas' => '1234567',
+                'jenis_pengguna' => 'karyawan',
+                'nama' => 'Dosen',
+                'password' => Hash::make('123'),
+                'jenis_kendaraan' => 'motor',
+                'no_plat' => 'BP12341A',
+                'role' => 'pengguna',
+                'status' => 'aktif',
+            ]
+        );
     }
 }
