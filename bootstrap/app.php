6a236ed30fbb31671faa195da04f2951a1485d9d
Greyari <159263690+Greyari@users.noreply.github.com>
2026-06-14 00:48:42 +0700

    namabhin di bootstrap untuk railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Tambahkan baris di bawah ini agar Laravel percaya semua proxy dari Railway
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
